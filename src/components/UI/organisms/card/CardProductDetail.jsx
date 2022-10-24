c6503aa4feab240e4153bbcadf0c1a6587bc5d36
danisec <daniaprilyanto35@gmail.com>
2022-10-24 15:49:31 +0700

    update CardProductDetail.jsx

--- src/components/UI/organisms/card/CardProductDetail.jsx
+++ src/components/UI/organisms/card/CardProductDetail.jsx
@@ -6,7 +6,7 @@ import ButtonAddToCart from '../../atoms/button/ButtonAddToCart'
 function CardProductDetail({ srcImg, altImg, title, description, price }) {
   return (
     <>
-      <div className='layout my-6 bg-white md:my-8'>
+      <div className='layout mt-6 bg-white md:my-8'>
         <div>
           <Breadcrumbs />
         </div>
@@ -27,22 +27,24 @@ function CardProductDetail({ srcImg, altImg, title, description, price }) {
               </p>
             </div>
 
-            <div className='mt-4'>
+            <div className='my-4 md:mt-4'>
               <span className='text-base font-bold text-gray-900 md:text-lg'>
                 Price <span className='text-sm'>$</span>
                 {price}
               </span>
             </div>
 
-            <div className='mt-8 flex items-center gap-12'>
-              <div className='mt-4 flex gap-12'>
-                <div className='flex items-center gap-4'>
-                  <ButtonCount />
+            <div className='sticky bottom-0 flex justify-center bg-white py-2 md:relative md:mt-8 md:justify-start md:py-0'>
+              <div className='flex items-center gap-12'>
+                <div className='m-0 flex gap-12 md:mt-4'>
+                  <div className='flex items-center gap-4'>
+                    <ButtonCount />
+                  </div>
                 </div>
-              </div>
 
-              <div className='mt-4'>
-                <ButtonAddToCart buttonName={'Add to Cart'} />
+                <div className='m-0 md:mt-4'>
+                  <ButtonAddToCart buttonName={'Add to Cart'} />
+                </div>
               </div>
             </div>
           </div>
