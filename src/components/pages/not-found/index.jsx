615d0bbe5fe88d61fac0e38693a1a6e223ab3131
danisec <daniaprilyanto35@gmail.com>
2022-10-26 19:25:22 +0700

    update index.jsx

--- src/components/pages/not-found/index.jsx
+++ src/components/pages/not-found/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 import NotFoundPage from './NotFoundPage'
 
 function index() {
   return (
     <>
+      <HelmetProvider>
+        <Helmet>
+          <title>BukaPedia - Page Not Found</title>
+        </Helmet>
+      </HelmetProvider>
+
       <NotFoundPage />
     </>
   )
