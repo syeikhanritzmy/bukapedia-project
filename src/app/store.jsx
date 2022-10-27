e0746bcbe1fe448c3b56791ab5e7c2f2775a5af2
danisec <daniaprilyanto35@gmail.com>
2022-10-27 17:36:17 +0700

    add authAdminReducer

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import homeReducer from '../features/home/homeSlice'
 import productDetailReducer from '../features/product-detail/productDetailSlice'
+import authAdminReducer from '../features/auth/admin/authAdminSlice'
 
 export const store = configureStore({
   reducer: {
     home: homeReducer,
     productDetail: productDetailReducer,
+    authAdmin: authAdminReducer,
   },
 })
