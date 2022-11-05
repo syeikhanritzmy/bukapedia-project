a4d053a6623fe3d26a02f6ff660d75de6242bfd6
syeikhanritzmy <syeikhanritzmy@gmail.com>
2022-11-05 21:07:04 +0800

    add product props

--- src/components/pages/home/HomePage.jsx
+++ src/components/pages/home/HomePage.jsx
@@ -1,29 +1,28 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-import { fetchProducts } from '../../../features/home/homeSlice'
-import Banner from '../../UI/organisms/banner/Banner'
-import Skeleton from '../../UI/atoms/skeleton/Skeleton'
-import ItemProduct from './ItemProduct'
-
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { fetchProducts } from '../../../features/home/homeSlice';
+import Banner from '../../UI/organisms/banner/Banner';
+import Skeleton from '../../UI/atoms/skeleton/Skeleton';
+import ItemProduct from './ItemProduct';
 function HomePage() {
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const allProducts = useSelector((state) => state.home.product)
-  const loading = useSelector((state) => state.home.loading)
+  const allProducts = useSelector((state) => state.home.product);
+  const loading = useSelector((state) => state.home.loading);
 
   useEffect(() => {
-    dispatch(fetchProducts())
-  }, [dispatch])
+    dispatch(fetchProducts());
+  }, [dispatch]);
 
   const loopSkeleton = () => {
-    return [1, 2, 3, 4, 5, 6, 7, 8].map((item) => <Skeleton key={item} />)
-  }
+    return [1, 2, 3, 4, 5, 6, 7, 8].map((item) => <Skeleton key={item} />);
+  };
 
   return (
     <>
-      <div className='md:layout my-8 overflow-hidden md:my-6'>
+      <div className="md:layout my-8 overflow-hidden md:my-6">
         {loading ? (
           <Skeleton />
         ) : (
@@ -31,14 +30,14 @@ function HomePage() {
         )}
       </div>
 
-      <div className='layout my-8 md:my-12'>
+      <div className="layout my-8 md:my-12">
         <div>
-          <h2 className='text-base font-bold text-gray-900 md:text-lg lg:text-xl'>
+          <h2 className="text-base font-bold text-gray-900 md:text-lg lg:text-xl">
             Products For You!
           </h2>
         </div>
 
-        <div className='mt-6 grid grid-cols-2 gap-4 bg-white ms:gap-8 sm:grid-cols-3 sm:gap-12 lg:grid-cols-4 lg:gap-16'>
+        <div className="mt-6 grid grid-cols-2 gap-4 bg-white ms:gap-8 sm:grid-cols-3 sm:gap-12 lg:grid-cols-4 lg:gap-16">
           {loading
             ? loopSkeleton()
             : allProducts?.map((product, index) => (
@@ -47,7 +46,7 @@ function HomePage() {
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
