733b06117761a2d3ce26f73f4e8e783a9b85165d
danisec <daniaprilyanto35@gmail.com>
2022-11-10 18:22:58 +0700

    add stockProductReducer

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import homeReducer from '../features/home/homeSlice'
 import productDetailReducer from '../features/product-detail/productDetailSlice'
 import cartReducer from '../features/cart/cartSlice'
+import stockProductReducer from '../features/stock-product/stockProductSlice'
 import authAdminReducer from '../features/auth/admin/authAdminSlice'
 
 export const store = configureStore({
@@ -9,6 +10,7 @@ export const store = configureStore({
     home: homeReducer,
     productDetail: productDetailReducer,
     cart: cartReducer,
+    stockProduct: stockProductReducer,
     authAdmin: authAdminReducer,
   },
 })
