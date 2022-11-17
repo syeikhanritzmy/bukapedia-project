b4764c3ef44accb9336d68b9d0353ce695f5dd08
danisec <daniaprilyanto35@gmail.com>
2022-11-17 15:13:31 +0700

    update handleAddToCart

--- src/components/UI/atoms/button/ButtonAddToCart.jsx
+++ src/components/UI/atoms/button/ButtonAddToCart.jsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { addToCart } from '../../../../features/cart/cartSlice';
-import toast, { Toaster } from 'react-hot-toast';
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { addToCart } from '../../../../features/cart/cartSlice'
+import toast, { Toaster } from 'react-hot-toast'
 
 function ButtonAddToCart({ buttonName, product, counter }) {
-  const notify = () => toast.success('success add to cart');
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const authUser = useSelector((state) => state.authUser)
+  const authAdmin = useSelector((state) => state.authAdmin)
+
+  const handleAddToCart = () => {
+    if (authUser.auth.token || authAdmin.auth.token) {
+      dispatch(addToCart({ ...product, cartQuantity: counter }))
+      toast.success('success add to cart')
+    } else {
+      navigate('/login')
+    }
+  }
+
   return (
     <>
       <button
-        className="h-10 w-28 rounded-full border border-black bg-transparent text-sm font-medium text-gray-900 hover:border-slate-100 hover:bg-slate-100"
-        onClick={() => {
-          dispatch(addToCart({ ...product, cartQuantity: counter }));
-          notify();
-        }}
+        className='h-10 w-28 rounded-full border border-black bg-transparent text-sm font-medium text-gray-900 hover:border-slate-100 hover:bg-slate-100'
+        onClick={handleAddToCart}
       >
         {buttonName}
       </button>
